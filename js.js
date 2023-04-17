b71f71f247f10497d3d7e49060ffaa29c1ac42cc
ANTRMX <killot86@gmail.com>
2023-04-17 09:20:45 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,22 +1,22 @@
-window.resizeTo(900,600); //Устанавливаем размер окна, и так понятно
+window.resizeTo(950,680); 								//Устанавливаем размер окна, и так понятно
 
-function OpenKeenetic(){								  //Функция которая получает из <input> номер подсети vlanid  
- var vlanid = document.getElementById('keenetic').value; // и подставляет его в ссылку. Затем открывает ссылку
+function OpenKeenetic(){							    //Функция которая получает из <input> номер подсети vlanid  
+ var vlanid = document.getElementById('vlan').value;    // и подставляет его в ссылку. Затем открывает ссылку
  var shopip = ('http://10.14.' + (vlanid) + '.1');		// но, пока не там где мне нужно((
  new ActiveXObject('WScript.Shell').Run(shopip);
 }
 
 function OpenUTM(){										//Такая же хуйня только открывает страницу УТМ
- var vlanid = document.getElementById('utm').value;
- var shopip = ('http://10.14.' + (vlanid) + '.2:8080');   //Дефолтный порт УТМ 8080
+ var vlanid = document.getElementById('vlan').value;
+ var shopip = ('http://10.14.' + (vlanid) + '.2:8080'); //Дефолтный порт УТМ 8080
  new ActiveXObject('WScript.Shell').Run(shopip);
 }
-function OpenPing(){ 			
- var vlanid = document.getElementById('ping').value;
- window.open('C:\windows\system32\cmd.exe' + (vlanid) + '-t');
-}
+
 function selectID() {										  //Эта функция получает число из <option> <select> 
 	var selectItem = document.getElementById('shops').value; // которое потом передает в <input>
-	document.getElementById('keenetic').value = selectItem; // кинетика 
-	document.getElementById('utm').value = selectItem; 	   // и УТМ
+	document.getElementById('vlan').value = selectItem; // vlan
 } 
+
+function Qalert() {
+	window.alert('Я пока ничего не умею');	// Затычка
+}
